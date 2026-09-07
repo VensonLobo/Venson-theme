00ca1b850639df1634fc11b8450d5c5c3b043b7c
VensonLobo <lobovenson1999@gmail.com>
2026-09-07 21:58:48 +0400

    Update global-header.tsx

--- components/global-header.tsx
+++ components/global-header.tsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { usePathname } from 'next/navigation';
-import { LogoLink } from './lobotravels-all-whitelogo.png';
+import { LogoLink } from '.public/lobotravels-all-whitelogo.png';
 import { COMPANY_DETAILS, CATEGORIES, DESTINATIONS } from '@/lib/data';
 import {
   Phone,
